3d8879a88bbfdf262e045e83b7a2bdaf8cb58b46
shruthanr <shruthan@outlok.com>
2021-07-25 01:26:08 +0530

    Add Collate class to dataset.py

--- dataset.py
+++ dataset.py
@@ -90,3 +90,15 @@ class FlickrDataset(Dataset):
 
         return img, caption_idx
 
+
+class Collate:
+    def __init__(self, pad_idx):
+        self.pad_idx = pad_idx
+
+    def __call__(self, batch):
+        imgs = [item[0].unsqueeze(0) for item in batch]
+        imgs = torch.cat(imgs, dim=0)
+        targets = [item[1] for item in batch]
+        targets = pad_sequence(targets, batch_first=False, padding_value=self.pad_idx)
+
+        return imgs, targets
